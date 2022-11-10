0bd64e1be112d4ef3d7644de9fc485f64c3e2c49
Christopher Shaw <silvertn350@yahoo.com>
2022-11-09 18:49:50 -0600

    2nd commit. Updated assignment

--- src.js
+++ src.js
@@ -4,19 +4,26 @@ class User {
         this.name = username
         this.email = emailAddress
         this.accountBalance = 0
+        return this
     }
     makeDeposit(amount){
         this.accountBalance += amount
+        return this
     }
     makeWithdrawal(amount){
         this.accountBalance -= amount
+        return this
+
     }
     displayBalance() {
         console.log('User:', this.name , 'Balance:', this.accountBalance)
+        return this
+
     }
     transferMoney(amount,username){
         this.accountBalance -= amount
         username.accountBalance += amount
+        return this
     }
 }
 
@@ -25,12 +32,17 @@ const Christopher = new User("Christopher","framedgamescreations@gmail.com")
 const Britt = new User("Britt","nomail@snailmail.org")
 const Biscuit = new User("Biscuit","IamAdOg@hotmail.com")
 
-Christopher.makeDeposit(100)
-Christopher.makeDeposit(500)
-Christopher.makeDeposit(500)
-Christopher.makeWithdrawal(200)
-Christopher.transferMoney(200,Britt)
-Christopher.displayBalance()
+// Christopher.makeDeposit(100)
+// Christopher.makeDeposit(500)
+// Christopher.makeDeposit(500)
+// Christopher.makeWithdrawal(200)
+// Christopher.transferMoney(200,Britt)
+// Christopher.displayBalance()
+
+
+Christopher.makeDeposit(100).makeDeposit(500).makeDeposit(500).makeWithdrawal(200).transferMoney(200,Britt).displayBalance()
+
+
 
 Britt.makeDeposit(700)
 Britt.makeDeposit(200)
